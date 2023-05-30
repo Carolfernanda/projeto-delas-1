601e573c889eec8fca630be09bb1ab18f6ff35cc
Carolfernanda <carolfern84@gmail.com>
2023-05-30 12:02:05 -0300

    atualizado

--- src/controllers/empreendedoraController.js
+++ src/controllers/empreendedoraController.js
@@ -19,7 +19,32 @@ class EmpreendedoraController {
                 res.status(201).send(empreendedora.toJSON())
             }
         })
-    };
+    }
+
+    static updateEmpreendedoras = (req, res) => {
+        const id = req.params.id;
+
+        empreendedoras.findByIdAndUpdate(id, { $set: req.body }, (err) => {
+            if (!err) {
+                res.status(200).send({ message: 'Empreendedora atualizada com sucesso' })
+            } else {
+                res.status(500).send({ message: err.message })
+            }
+        })
+    }
+
+
+    static deleteEmpreendedoras = (req, res) => {
+        const id = req.params.id;
+
+        empreendedoras.findByIdAndDelete(id, (err) => {
+            if (!err) {
+                res.status(200).send({ message: 'Empreendedora removida com sucesso' })
+            } else {
+                res.status(500).send({ message: err.message })
+            }
+        })
+    }
 };
 
 
--- src/routes/empreendedora.js
+++ src/routes/empreendedora.js
@@ -3,7 +3,9 @@ const EmpreendedoraController = require ("../controllers/empreendedoraController
 
 const router = express.Router();
 
-router.get ("/", EmpreendedoraController.getAllEmpreendedoras);
-router.post("/", EmpreendedoraController.createEmpreendedoras);
+router.get ("/", EmpreendedoraController.getAllEmpreendedoras)
+router.post("/", EmpreendedoraController.createEmpreendedoras)
+router.put("/:id", EmpreendedoraController.updateEmpreendedoras)
+router.delete("/:id", EmpreendedoraController.deleteEmpreendedoras)
 
 module.exports = router;
