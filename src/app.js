d6e5b615bd73a83a94164dd16653f9183998ff92
Seu nome <111187449+Carolfernanda@users.noreply.github.com>
2023-06-08 12:33:24 -0300

    feito atualização

--- src/app.js
+++ src/app.js
@@ -2,14 +2,15 @@ const express = require ("express");
 const index = require("./routes/index");
 const db = require ("./config/dbConnect.js");
 const empreendedoras = require("./routes/empreendedora");
-const clientes = require ("./routes/cliente");
+const clientes = require ("./routes/cliente"); // fazer importação de clientes - feito
+
 const app = express ();
 
 
 app.use (express.json());
 app.use("/", index);
 app.use("/empreendedoras", empreendedoras);
-app.use("/clientes", clientes);
+app.use("/clientes", clientes); //feito endpoint clientes
 
 
 db.on("error", console.log.bind(console, 'Erro de conexão'))
