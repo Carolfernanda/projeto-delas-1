b4cf8f5824c37bb6f49166e3b8e2e512735a015a
Carolfernanda <carolfern84@gmail.com>
2023-06-09 12:09:06 -0300

    atualização

--- src/routes/cliente.js
+++ src/routes/cliente.js
@@ -3,7 +3,12 @@ const ClienteController = require ("../controllers/clienteControllers");
 
 const router = express.Router();
 
-router.get ("/", ClienteController.getAllClientes);
-router.post("/", ClienteController.createClientes);
+router.get ("/", ClienteController.getAllClientes)
+router.post("/", ClienteController.createClientes)
+router.put("/:id", ClienteController.updateClientes)
+router.delete("/:id", ClienteController.deleteClientes)
+router.get("/:id", ClienteController.getByIdClientes)
+
+
 
 module.exports = router;
